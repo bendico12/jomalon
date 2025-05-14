f813378bae3523b5052928823752a0b23c29b02c
bendico <73071322+bendico12@users.noreply.github.com>
2025-05-14 22:09:57 +0900

    Create viewport-fix.js

---
+++ java/viewport-fix.js
@@ -0,0 +1,8 @@
+function setViewportHeight() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+  
+  window.addEventListener('load', setViewportHeight);
+  window.addEventListener('resize', setViewportHeight);
+  
